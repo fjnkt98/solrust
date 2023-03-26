9d1b7cb66423857c32ab1a69090f3945ebb7507a
fjnkt98 <fjnkt98@gmail.com>
2023-03-26 10:26:37 +0900

    feat: sanitize search keyword

--- solrust_derive_internals/src/lib.rs
+++ solrust_derive_internals/src/lib.rs
@@ -130,7 +130,7 @@ pub fn impl_dismax_query_parser(input: TokenStream) -> TokenStream {
         impl SolrDisMaxQueryBuilder for #struct_name {
             fn q(mut self, q: String) -> Self {
                 // TODO: 引数の型を抽象モデルに変更する
-                self.params.insert("q".to_string(), q.to_string());
+                self.params.insert("q".to_string(), self.sanitize(&q).to_string());
                 self
             }
 
